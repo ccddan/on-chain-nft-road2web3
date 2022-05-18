244821bb19c0d9b1af14f36d414a928750084319
ccddan <ccdd.dev@gmail.com>
2022-05-17 21:27:54 -0500

    sc:feat: chain battles contract
    Smart contract with deployment script, artifacts and docs

--- smart-contracts/scripts/deploy.ts
+++ smart-contracts/scripts/deploy.ts
@@ -1,12 +1,42 @@
 import { ethers } from "hardhat";
+import { join } from "path";
+import { writeFileSync } from "fs";
 
 async function main() {
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const contractName = "ChainBattles";
+  const ChainBattles = await ethers.getContractFactory(contractName);
+  const chainBattles = await ChainBattles.deploy();
 
-  await greeter.deployed();
+  await chainBattles.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log(`${contractName} deployed to:`, chainBattles.address);
+
+  const exportFile = join(
+    __dirname,
+    "..",
+    "artifacts",
+    "contracts",
+    `${contractName}.sol`,
+    `${contractName}.addr.json`
+  );
+  const exportFileTimestamp = exportFile.replace(
+    ".addr.",
+    `.addr-${new Date().toISOString()}.`
+  );
+  const fileData = JSON.stringify(
+    { name: contractName, addr: chainBattles.address },
+    null,
+    2
+  );
+  writeFileSync(exportFile, fileData, { flag: "w" });
+  writeFileSync(exportFileTimestamp, fileData);
+
+  console.log(
+    "Contract info exported at:\n\t",
+    exportFile,
+    "\n\t",
+    exportFileTimestamp
+  );
 }
 
 main().catch((error) => {
